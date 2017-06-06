e224f1a24a69ec88395d436b660d8447026a166b
annelliott <1842714077@qq.com>
2017-06-06 09:10:48 +0800

    include dp

--- Algh_newcoder/src/recursionANDdp/PrimaryFibonacci.java
+++ Algh_newcoder/src/recursionANDdp/PrimaryFibonacci.java
@@ -1,5 +1,9 @@
 package recursionANDdp;
-
+/*
+ * 	如果递推公式满足f(n) = a*f(1) + b*f(2) + ... + g*f(n-1)
+ * 	则可以利用状态矩阵来实现。
+ * 	时间复杂度为O(log(n))
+ * */
 	// 斐波那契数列的形式：f(n) = f(n-1) + f(n-2)
 	// 二阶递推公式可以转化为状态矩阵乘以参数。
 	// 斐波那契数列可以表示为{1,1;1,0}
@@ -51,7 +55,9 @@ public class PrimaryFibonacci {
 		// m为被乘的矩阵，res保存从0位到当前位的矩阵
 		int[][] tmp = m;
 		for (; p != 0; p = (p >> 1)) {
+			// 计算当前位是否为0
 			if ((p & 1) != 0) {
+				// 如果不是，则利用前面的值乘以当前的值
 				res = muliMatrix(res, tmp);
 			}
 			// tmp存储当前位对应的m的2的倍数。
@@ -82,8 +88,42 @@ public class PrimaryFibonacci {
 		}
 		int[][] base = {{1,1},{1,0}};
 		int[][] res = matrixPower(base, n - 2);
-		// res[0][0]表示
+		// 利用(f(2),f(1)) * base = (f(n),f(n-1))求得f(n)
 		return res[0][0] + res[1][0];
+	}
+	// 上台阶，一次只能上一个或两个。则第n个台阶只有两种情况，S(n) = S(n-1) + S(n-2)
+	// S[1] = 1,S[2] = 2;base = {{1,1},{1,0}}
+	public static int S3(int n) {
+		if (n < 1) {
+			return 0;
+		}
+		if (n == 1 || n == 2) {
+			return n;
+		}
+		int[][] base = {{1,1},{1,0}};
+		int[][] res = matrixPower(base, n - 2);
+		return 2 * res[0][0] + res[1][0];
+	}
+	/*	第一年只有一头母猪，并且母猪不会死。母猪每年都会生一只小母猪
+	 * 	小母猪过了三年后才会成熟生小母猪 
+	 * 	问第N年时，一共有多少头猪。
+	 * 	第N年的猪的数量是由第N-1年和第N-3年成熟的那些猪所下的猪仔所决定的。
+	 * 	S(1) = 1; S(2) = 2; S(3) = 3;S(4) = 4;S(5) = 6;
+	 * 	S(N) = S(N-1) + S(N-3),三阶递推数列可以用3*3矩阵来表示
+	 * 	状态转移矩阵为{{1,1,0},{0,0,1},{1,0,0}}
+	 * */
+	public static int pigNum(int n) {
+		if (n < 1) {
+			return 0;
+		}
+		if (n == 1 || n == 2 || n == 3) {
+			return n;
+		}
+		// 状态转移矩阵base
+		int[][] base = {{1,1,0},{0,0,1},{1,0,0}};
+		int[][] res = matrixPower(base, n - 3);
+		// 二维矩阵res存结果
+		return 3 * res[0][0] + 2 * res[1][0] + res[2][0];
 	}
 	public static void main(String[] args) {
 		System.out.println(f3(4));
